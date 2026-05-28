e85f9ceb79d0175dd9455d674d68d5187e5d724e
Ashik <lllashik2@gmail.com>
2026-05-28 22:39:51 +0530

    fix: make username optional in RegisterSerializer to allow blank usernames

--- backend/apps/accounts/serializers.py
+++ backend/apps/accounts/serializers.py
@@ -15,6 +15,7 @@ class UserSerializer(serializers.ModelSerializer):
 
 class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
+    username = serializers.CharField(required=False, allow_blank=True)
     
     class Meta:
         model = User
